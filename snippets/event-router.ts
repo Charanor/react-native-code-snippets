256f31dbab1ffaf1abcee2aa3df85ce1561528b3
Jesper Sporron <jesper.sporron@gmail.com>
2020-04-10 13:41:11 +0200

    Add event router code snippet.

---
+++ snippets/event-router.ts
@@ -0,0 +1,82 @@
+/**
+ * Author: Jesper Sporron
+ * Author GitHub: https://github.com/Charanor
+ * Project Github: https://github.com/Charanor/react-native-code-snippets
+ * License: Creative Commons Zero v1.0 Universal (CC0)
+ * Required extra node modules: nothing
+ * 
+ * Usage:
+ *     import EventRouter from "./event-router";
+ * 
+ *     type State = "one" | "two";    
+ * 
+ *     class ClassWithEvent {
+ *         private state: State = "one";
+ *         private onStateChangedRouter: EventRouter<State>;
+ * 
+ *         public setState(state: State) {
+ *             this.state = state;
+ *             this.onStateChangedRouter.trigger(state);
+ *         }
+ * 
+ *         public get onStateChanged() {
+ *             return this.onStateChangedRouter.asEventFuncs();
+ *         }
+ *     }
+ * 
+ *     const instance = new ClassWithEvent();
+ * 
+ *     function SomeComponent() {
+ *        const [state, setState] = useState<State>(undefined);
+ *        useEffect(() => {
+ *            const sub = instance.onStateChanged.subscribe(setState);
+ *            return sub.remove;
+ *        }, []);
+ *  
+ *        return (
+ *            <View>
+ *                <Text>{state ?? "No state yet..."}</Text>
+ *                <TouchableOpacity onPress={() => instance.setState("two")}>
+ *                    <Text>Set state</Text>
+ *                </TouchableOpacity>
+ *            </View>
+ *        );
+ *     }
+ * 
+ * Please keep this comment at the top of the file to show support (even though you are free to remove it) :)
+ */
+
+type EventListener<T> = (data?: T) => void;
+type EventSubscription = { remove: () => void };
+
+interface EventFuncs<T> {
+    subscribe(listener: EventListener<T>): EventSubscription;
+}
+
+class EventRouter<T> implements EventFuncs<T> {
+    private listeners: EventListener<T>[] = [];
+
+    subscribe(listener: EventListener<T>) {
+        this.listeners.push(listener);
+        return {
+            remove: () => {
+                const idx = this.listeners.indexOf(listener);
+                this.listeners.splice(idx, 1);
+            }
+        };
+    }
+
+    public trigger(data?: T) {
+        this.listeners.forEach(l => l(data));
+    }
+
+    public asEventFuncs(): EventFuncs<T> {
+        return this;
+    }
+}
+
+export default EventRouter;
+export {
+    EventListener,
+    EventSubscription
+};
